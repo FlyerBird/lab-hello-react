bfbca500649783548c294f4572f510bc958f3eb3
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-07-21 23:05:55 +0200

    loadedImageIt4

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import logo from '../images/ironhack-logo-xs.png'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+  <img src= {logo} alt="Ironhack Logo" />
+    </nav>
+  )
+}
+
+export default Navbar;
