6788c9d52ff003ee3d53f9bce37fddf6e14a8003
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-07-22 13:35:28 +0200

    labDone

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import icon1 from '../images/icon1.png'
+import icon2 from '../images/icon2.png'
+import icon3 from '../images/icon3.png'
+import icon4 from '../images/icon4.png'
 
 const Main = () => {
   return (
-   <div>
-    <img className='icon1'  src= {icon1} alt="Declarative icon" />
+   <div className='main'>
+
+    <div className='main1'>
+      <img className='icon1'  src= {icon1} alt="Declarative icon" />
+      <h1>Declarative</h1>
+      <p>React makes it <br></br> painless to create <br></br> interactive UIs.</p>
+    </div>
+
+    <div className='main2'>
+      <img className='icon2'  src= {icon2} alt="Components icon" />
+      <h1>Components</h1>
+      <p>Build encapsulated<br></br>components that<br></br>manage their state.</p>
+    </div>
+
+    <div className='main3'>
+      <img className='icon3'  src= {icon3} alt="Single way icon" />
+      <h1>Single-Way</h1>
+      <p>A set of immutable<br></br>values are passed to<br></br>the component's.</p> 
+    </div>
+    
+
+    <div className='main4'>
+      <img className='icon4'  src= {icon4} alt="Jsx icon" />
+      <h1>JSX</h1>
+      <p>Statically-typed, <br></br> designed to run on <br></br> modern browsers.</p>
+    </div>
+    
    </div>
        
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 
 import React from 'react';
-import logo from '../images/ironhack-logo-xs.png'
+import logo from '../images/ironhack-logo-xs.png';
+import menu from '../images/menu-top-xs.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <img className='App-logo'  src= {logo} alt="Ironhack Logo" />
+        <div className='top'>
+          <img className='App-logo'  src= {logo} alt="Ironhack Logo" />
+          <img className='Burger'  src= {menu} alt="Burger Menu" />
+        </div>
+        
         <h1>Say Hello to <br></br> ReactJS</h1>
         <p>You will learn how to use <br></br>
             the most popular frontend library, <br></br>
